314d92c548370270afe3c9e9f2eef8cfe03bc976
Adepiper <52428747+Adepiper@users.noreply.github.com>
2019-12-09 01:27:21 +0100

    Jquery changes

--- js/links.js
+++ js/links.js
@@ -32,4 +32,14 @@ twitter.addEventListener('click', twitterLink)
    // }
 }
 
+document.querySelector('.button').onmousemove = (e) => {
+
+	const x = e.pageX - e.target.offsetLeft
+	const y = e.pageY - e.target.offsetTop
+
+	e.target.style.setProperty('--x', `${ x }px`)
+	e.target.style.setProperty('--y', `${ y }px`)
+	
+}
+
 links();
